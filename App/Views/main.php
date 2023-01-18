ce4ae603b20bd882500b7df6ce8f4569cb112b45
Alagesan <alagesanbe08@gmail.com>
2023-01-18 17:15:12 +0530

    - Fix: button css color

--- App/Views/main.php
+++ App/Views/main.php
@@ -14,21 +14,22 @@ defined('ABSPATH') or die;
     <div class="wlt_content">
         <div class="wlt-sections">
             <div class="title">
-                <h3><?php _e('Loco Translate', 'wp-loyalty-translate'); ?></h3>
+                <h3><?php _e('Loco Translate:', 'wp-loyalty-translate'); ?></h3>
             </div>
             <div class="content">
-                <p><?php _e('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cumque eaque, facilis laboriosam mollitia natus nobis perferendis possimus quas totam? Asperiores in ipsam omnis provident? Ad adipisci animi minus tempora.', 'wp-loyalty-translate'); ?></p>
+                <p><?php echo sprintf(__('You can now translate the dynamic content like Campaign title, descriptions, reward title, description, content inside the Launcher widget easily. Follow this step by step guide for <a href="%s" target="_blank">translating the strings with LocoTranslate</a>', 'wp-loyalty-translate'), 'https://docs.wployalty.net/quick-start-guides/translating-wployalty-with-locotranslate'); ?></p>
             </div>
         </div>
         <div class="wlt-sections">
             <div class="title">
-                <h3><?php _e('WPML Translate', 'wp-loyalty-translate'); ?></h3>
+                <h3><?php _e('WPML Translate:', 'wp-loyalty-translate'); ?></h3>
             </div>
             <div class="content">
-                <p><?php _e('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cumque eaque, facilis laboriosam mollitia natus nobis perferendis possimus quas totam? Asperiores in ipsam omnis provident? Ad adipisci animi minus tempora.', 'wp-loyalty-translate'); ?></p>
+                <p><?php echo sprintf(__('You can now translate the dynamic content like Campaign title, descriptions, reward title, description, content inside the Launcher widget easily. Follow this step by step guide for <a href="%s" target="_blank">translating the strings with WPML</a>', 'wp-loyalty-translate'), 'https://docs.wployalty.net/translating-wployalty/translating-wployalty-strings-using-wpml'); ?></p>
                 <?php if (isset($is_wpml_translate_string_available) && $is_wpml_translate_string_available): ?>
                     <div class="wlt_button">
                         <a class="wlt_wpml_button" id="wlt_update_wpml_string"
+                           style="background-color: #4747EB;padding: 8px 12px;color: #FFF;border-radius: 6px;"
                         ><?php _e('Update Dynamic String for WPML', 'wp-loyalty-translate'); ?></a>
                     </div>
                 <?php endif; ?>
