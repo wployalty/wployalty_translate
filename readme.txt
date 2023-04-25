cb0553937a5fc611e63a429fa60f1a817e8c5d48
Alagesan <alagesanbe08@gmail.com>
2023-04-25 14:30:04 +0530

    - readme file

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== WPLoyalty - dynamic string translation ===
+Contributors: wployalty
+Donate link: https://wployalty.net
+Tags: points, rewards, loyalty, woocommerce points and rewards, points and rewards, woocommerce loyalty, woocommerce rewards, woocommerce reward program, woocommerce loyalty program, reward system
+Requires at least: 4.9.0
+Tested up to: 6.2
+WC requires at least: 6.5.0
+WC tested up to: 7.5.0
+Stable tag: 1.2.1
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WPLoyalty - dynamic string translation add-on used to translate dynamic string for WPLoyalty and related add-on
+
+== Description ==
+
+[WPLoyalty - dynamic string translation](https://wployalty.net?utm_campaign=wplfree&utm_source=translate_addon&utm_medium=listing) used for translate string automatically.
+
+== Installation ==
+
+Just use the WordPress installer or upload to the /wp-content/plugins folder. Then Activate the WPLoyalty - Auto currency plugin.
+More information could be found in the documentation
+
+= Minimum Requirements =
+
+    - WordPress 4.9.0 or greater
+    - WooCommerce 6.5.0 or greater
+    - WPLoyalty 1.1.9 or greater
+    - PHP version 7.0.0 or greater
+
+== Changelog ==
+= 1.0.0 =
+- initial release
+
+= 1.0.1 =
+- Fix: Free version support
+
+= 1.0.2 =
+- Fix: version update check
--- wp-loyalty-translate.php
+++ wp-loyalty-translate.php
@@ -3,14 +3,14 @@
  * Plugin Name: WPLoyalty - dynamic string translation
  * Plugin URI: https://www.wployalty.net
  * Description: This add-on used to translate dynamic string for WPLoyalty and related add-on
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Wployalty
  * Slug: wp-loyalty-translate
  * Text Domain: wp-loyalty-translate
  * Domain Path: /i18n/languages/
  * Requires at least: 4.9.0
  * WC requires at least: 6.5
- * WC tested up to: 7.3
+ * WC tested up to: 7.5
  * Contributors: Wployalty, Alagesan
  * Author URI: https://wployalty.net/
  * License: GNU General Public License v3.0
@@ -20,7 +20,7 @@
  */
 defined('ABSPATH') or die;
 defined('WLT_PLUGIN_NAME') or define('WLT_PLUGIN_NAME', 'WPLoyalty - Dynamic string translation');
-defined('WLT_PLUGIN_VERSION') or define('WLT_PLUGIN_VERSION', '1.0.1');
+defined('WLT_PLUGIN_VERSION') or define('WLT_PLUGIN_VERSION', '1.0.2');
 defined('WLT_PLUGIN_SLUG') or define('WLT_PLUGIN_SLUG', 'wp-loyalty-translate');
 defined('WLT_PLUGIN_URL') or define('WLT_PLUGIN_URL', plugin_dir_url(__FILE__));
 // Define plugin path
@@ -53,6 +53,12 @@ if (isWployaltyActiveOrNot()) {
         require __DIR__ . '/vendor/autoload.php';
     }
     if (class_exists(\Wlt\App\Router::class)) {
+        $myUpdateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+            'https://github.com/wployalty/wployalty_translate',
+            __FILE__,
+            'wp-loyalty-translate'
+        );
+        $myUpdateChecker->getVcsApi()->enableReleaseAssets();
         $plugin = new \Wlt\App\Router();
         if (method_exists($plugin, 'initHooks')) $plugin->initHooks();
     }
