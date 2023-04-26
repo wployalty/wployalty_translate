b4bcffcfb61c0c8d831187fa19cfa2eb4a51215f
Alagesan <alagesanbe08@gmail.com>
2023-04-26 10:31:26 +0530

    - free support

--- App/Router.php
+++ App/Router.php
@@ -14,7 +14,7 @@ class Router
     {
         self::$controller = empty(self::$controller) ? new Controller() : self::$controller;
         add_filter('wlr_loyalty_apps', array(self::$controller, 'getAppDetails'));
-        if (is_admin() && (self::$controller->isPluginIsActive('wp-loyalty-rules/wp-loyalty-rules.php') || self::$controller->isPluginIsActive('wp-loyalty-rules-lite/wp-loyalty-rules-lite.php'))) {
+        if (is_admin() && (self::$controller->isPluginIsActive('wployalty/wp-loyalty-rules-lite.php') || self::$controller->isPluginIsActive('wp-loyalty-rules/wp-loyalty-rules.php') || self::$controller->isPluginIsActive('wp-loyalty-rules-lite/wp-loyalty-rules-lite.php'))) {
             add_action('admin_menu', array(self::$controller, 'adminMenu'));
             add_action('admin_footer', array(self::$controller, 'menuHide'));
             add_action('admin_enqueue_scripts', array(self::$controller, 'adminScripts'), 100);
